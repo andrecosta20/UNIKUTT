64e93ef66db3bcfa00c8f8df48af4ffb28a7e6a3
andrecosta20 <84419353+andrecosta20@users.noreply.github.com>
2021-05-18 18:24:36 -0300

    Create Aplicacao.java

---
+++ Aplicacao.java
@@ -0,0 +1,16 @@
+package Executar;
+
+import Logica.*;
+
+public class Aplicacao {
+	/*
+	 * @author Lucas
+	 */
+
+
+	public static void main(String[] args) {
+		Unikut social = new Unikut();
+
+		social.menuStart();
+	}
+}
